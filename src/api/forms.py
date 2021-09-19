1bd1d9bad0f38b8d08005bddb979fe57de9c85d1
whitedevil31 <magz3116@gmail.com>
2021-09-19 13:39:18 +0530

    Setup initail forms

---
+++ src/api/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class NameForm(forms.Form):
+    url = forms.CharField(label='Your hackathon URL', max_length=100,widget=forms.TextInput(attrs={'placeholder': 'Search'}))
+    email = forms.CharField(label='Your email', max_length=100,widget=forms.TextInput(attrs={'placeholder': 'email'}))
